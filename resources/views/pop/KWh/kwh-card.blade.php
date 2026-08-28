2afebdd079d838acc6e755db14c70d74e66cef36
rsmgf <gerdofiero@email.com>
2026-08-28 16:45:34 +0700

    renamed rectifier-card.css to card.css

--- resources/views/pop/KWh/kwh-card.blade.php
+++ resources/views/pop/KWh/kwh-card.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap">
 
-    @vite(['resources/css/sidebar.css', 'resources/css/rectifier-card.css'])
+    @vite(['resources/css/sidebar.css', 'resources/css/card.css'])
 </head>
 
 <body>
